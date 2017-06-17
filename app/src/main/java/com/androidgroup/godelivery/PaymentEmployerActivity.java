3680ebf24d8fe422da88a64ab2982d8c27da2623
shehrya <khanshehryar362@gmail.com>
2017-06-17 18:49:26 +0200

    Payment Employer Activity again
    .Java libraries are added

---
+++ app/src/main/java/com/androidgroup/godelivery/PaymentEmployerActivity.java
@@ -0,0 +1,40 @@
+package com.androidgroup.godelivery;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.Typeface;
+import android.net.Uri;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class PaymentEmployerActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.payment_job_employer);
+
+    }
+
+
+
+}
