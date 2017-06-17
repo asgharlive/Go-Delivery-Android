95c67c03511675b704634f514f616ef245683c0f
shehrya <khanshehryar362@gmail.com>
2017-06-17 19:28:27 +0200

    Pre Job Photo Worker Activity again
    .Java class camera implementation completely done

--- app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
+++ app/src/main/java/com/androidgroup/godelivery/PreJobPhotoWorkerActivity.java
@@ -93,8 +93,253 @@ public class PreJobPhotoWorkerActivity extends Activity {
         setContentView(R.layout.pre_photo_worker);
 
 
+        Intent intent = getIntent();
+        JobID = intent.getStringExtra("AcceptedJobIDNumber");
+
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "FancyFont_1.ttf");
+
+        jobStatusTitle = (TextView) findViewById(R.id.PreJobSeekerStatusTitleID);
+
+        jobStatus = (TextView) findViewById(R.id.PreJobSeekerStatusID);
+
+        progressBar = (ProgressBar) findViewById(R.id.PrePhotoSeekerProgressBarID);
+
+        imageView = (ImageView) findViewById(R.id.PrePhotoSeekerImageID);
+
+        textDescription = (TextView) findViewById(R.id.PrePhotoSeekerTextDescriptionID);
+
+        startJobButton = (Button) findViewById(R.id.startJobButtonID);
+
+        jobDetailsButton = (Button) findViewById(R.id.PreJobSeekerJobDetailsButtonID);
+
+        LogOutButton = (Button) findViewById(R.id.LogoutButtonID);
+
+        LogOutButton.setTypeface(font);
+        LogOutButton.setTextColor(Color.WHITE);
+
+        RefreshButton = (Button) findViewById(R.id.RefreshButtonID);
+
+        RefreshButton.setTypeface(font);
+        RefreshButton.setTextColor(Color.WHITE);
+
+
+        jobStatusTitle.setTypeface(font);
+        jobStatusTitle.setTextColor(Color.WHITE);
+
+        jobStatus.setTypeface(font);
+        jobStatus.setTextColor(Color.WHITE);
+
+        textDescription.setTypeface(font);
+        textDescription.setTextColor(Color.WHITE);
+
+        startJobButton.setTypeface(font);
+        startJobButton.setTextColor(Color.WHITE);
+
+        jobDetailsButton.setTypeface(font);
+        jobDetailsButton.setTextColor(Color.WHITE);
+
+
+        for (int i = 0; i < jobDetails.length; ++i) {
+            jobDetails[i] = "";
+
+        }
+
+        jobFileName = (JobID + ".txt");
+
+        new FetchAcceptedJobDetails().execute("http://192.168.0.185/AndroidApps/GoDelivery/AcceptedJobs/" + jobFileName);
 
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == 100 && resultCode == RESULT_OK) {
+
+            Bitmap bitmapPhoto = (Bitmap) data.getExtras().get("data");
+
+            imageView.setImageBitmap(bitmapPhoto);
+
+            ByteArrayOutputStream bao = new ByteArrayOutputStream();
+            bitmapPhoto.compress(Bitmap.CompressFormat.JPEG, 90, bao);
+            byte[] ba = bao.toByteArray();
+            ba1 =  Base64.encodeToString(ba, Base64.DEFAULT);
+
+            // Upload image to server
+            new SendPreJobPhotoToServer().execute("http://192.168.0.185/AndroidApps/GoDelivery/PreJobPhotos/PreJobPhotoUpload.php");
+
+
+        }
+
+
+
+    }
+
+
+
+
+    public void jobDetailsPrePhotoSeekerButton(View v)
+    {
+
+        Typeface font = Typeface.createFromAsset(getAssets(), "FancyFont_1.ttf");
+
+
+        LayoutInflater inflater = getLayoutInflater();
+        View dialoglayout = inflater.inflate(R.layout.view_job_details, null);
+
+
+
+        TextView jobTitleTextView = (TextView) dialoglayout.findViewById(R.id.GeneralJobTitleID);
+        TextView distanceTextView = (TextView) dialoglayout.findViewById(R.id.GeneralDistanceID);
+        TextView rateTextView = (TextView) dialoglayout.findViewById(R.id.GeneralRateID);
+        TextView weightTextView = (TextView) dialoglayout.findViewById(R.id.GeneralWeightID);
+        TextView dimensionsTextView = (TextView) dialoglayout.findViewById(R.id.GeneralDimensionID);
+        TextView jobCreaterNameTextView = (TextView) dialoglayout.findViewById(R.id.GeneralJobOperaterNameID);
+        TextView jobCreaterContactNoTextView = (TextView) dialoglayout.findViewById(R.id.GeneralJobOperatorNumberID);
+
+        TextView pickUpAddressTextView = (TextView) dialoglayout.findViewById(R.id.GeneralPickUpPersonAddressID);
+        TextView deliveryAddressTextView = (TextView) dialoglayout.findViewById(R.id.GeneralDeliveryPersonAddressID);
+
+        TextView pickUpPersonNameTextView = (TextView) dialoglayout.findViewById(R.id.GeneralPickUpPersonNameID);
+        TextView pickUpPersonContactNumberTextView = (TextView) dialoglayout.findViewById(R.id.GeneralPickUpPersonContactNumberID);
+
+        TextView deliveryPersonNameTextView = (TextView) dialoglayout.findViewById(R.id.GeneralDeliveryPersonNameID);
+        TextView deliveryPersonContactNumberTextView = (TextView) dialoglayout.findViewById(R.id.GeneralDeliveryPersonContactNumberID);
+
+
+        jobTitleTextView.setTypeface(font);
+        jobTitleTextView.setTextColor(Color.BLACK);
+
+        distanceTextView.setTypeface(font);
+        distanceTextView.setTextColor(Color.BLACK);
+
+        rateTextView.setTypeface(font);
+        rateTextView.setTextColor(Color.BLACK);
+
+        weightTextView.setTypeface(font);
+        weightTextView.setTextColor(Color.BLACK);
+
+        dimensionsTextView.setTypeface(font);
+        dimensionsTextView.setTextColor(Color.BLACK);
+
+        jobCreaterNameTextView.setTypeface(font);
+        jobCreaterNameTextView.setTextColor(Color.BLACK);
+
+        jobCreaterContactNoTextView.setTypeface(font);
+        jobCreaterContactNoTextView.setTextColor(Color.BLACK);
+
+        pickUpAddressTextView.setTypeface(font);
+        pickUpAddressTextView.setTextColor(Color.BLACK);
+
+        deliveryAddressTextView.setTypeface(font);
+        deliveryAddressTextView.setTextColor(Color.BLACK);
+
+
+
+        pickUpPersonNameTextView.setTypeface(font);
+        pickUpPersonNameTextView.setTextColor(Color.BLACK);
+
+
+        pickUpPersonContactNumberTextView.setTypeface(font);
+        pickUpPersonContactNumberTextView.setTextColor(Color.BLACK);
+
+
+        deliveryPersonNameTextView.setTypeface(font);
+        deliveryPersonNameTextView.setTextColor(Color.BLACK);
+
+
+        deliveryPersonContactNumberTextView.setTypeface(font);
+        deliveryPersonContactNumberTextView.setTextColor(Color.BLACK);
+
+
+
+
+
+        Double distanceDoubleFormat = Double.valueOf(jobDetails[7]);
+        double roundDistance = (double) Math.round(distanceDoubleFormat * 100.0) / 100.0;
+        String roundedDistance = String.valueOf(roundDistance);
+
+        Double amountDoubleFormat = Double.valueOf(jobDetails[8]);
+        double roundAmount = (double) Math.round(amountDoubleFormat * 100.0) / 100.0;
+        String roundedAmount = String.valueOf(roundAmount);
+
+
+
+
+
+        jobTitleTextView.setText("Job Title\n" + jobDetails[9]);
+
+        distanceTextView.setText("Distance\n" + roundedDistance + " KM");
+        rateTextView.setText("Cost\n€" + roundedAmount);
+        weightTextView.setText("Weight\n" + jobDetails[10] + " KG");
+        dimensionsTextView.setText("Size/Dimension (LxWxH)\n" + jobDetails[11] + " inches");
+
+
+
+        jobCreaterNameTextView.setText("Job Creater Name\n" + jobDetails[1]);
+        jobCreaterContactNoTextView.setText("Job Creater Contact Number\n" + jobDetails[5]);
+
+
+        pickUpAddressTextView.setText("Pick Up Address\n" + jobDetails[14]);
+        deliveryAddressTextView.setText("Delivery Address\n" + jobDetails[19]);
+
+        pickUpPersonNameTextView.setText("Pick Up Person Name\n" + jobDetails[12]);
+        pickUpPersonContactNumberTextView.setText("Pick Up Person Contact Number\n" + jobDetails[13]);
+
+        deliveryPersonNameTextView.setText("Delivery Person Name\n" + jobDetails[17]);
+        deliveryPersonContactNumberTextView.setText("Delivery Person Contact Number\n" + jobDetails[18]);
+
+
+        builder = new AlertDialog.Builder(this);
+
+        builder.setView(dialoglayout);
+        builder.setTitle(JobID);
+
+        builder.setNeutralButton("Close", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                alertDw.dismiss();
+            }
+        });
+
+
+        alertDw = builder.create();
+
+
+        alertDw.show();
+
+
+
+    }
+
+    public void startJobPrePhotoSeekerButton(View v)
+    {
+
+        if (getApplicationContext().getPackageManager().hasSystemFeature(
+                PackageManager.FEATURE_CAMERA)) {
+
+            textDescription.setVisibility(View.GONE);
+            // Open default camera
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            intent.putExtra(MediaStore.EXTRA_OUTPUT, fileUri);
+
+            // start the image capture Intent
+            startActivityForResult(intent, 100);
+
+        } else {
+            Toast.makeText(getApplication(), "Camera not supported", Toast.LENGTH_LONG).show();
+        }
+
+    }
+
+
+
+
+
+
+
+}
 
 
 }
